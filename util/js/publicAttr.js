52a9b5fa39b3a28e369cac976f3ba75f08cd9e7c
fengyy51 <694590074@qq.com>
2017-08-21 15:27:29 +0800

    投票页面加授权

--- util/js/publicAttr.js
+++ util/js/publicAttr.js
@@ -1,12 +1,12 @@
-// urlServer="http://169.254.79.138:8085";  //线下
+// urlServer="http://169.254.171.3:8085";  //线下
 
-urlServer="http://120.27.227.32:8085";   //线上
-var urlYuming="http://fbinwang.bw158.cn"; //线上
-var APPIDall="wx1ce88e01dd6e9a77";//宾王
+// urlServer="http://120.27.227.32:8085";   //线上
+// var urlYuming="http://fbinwang.bw158.cn"; //线上
+// var APPIDall="wx1ce88e01dd6e9a77";//宾王
 
-// urlServer="http://120.27.227.32:8075"; //线上测试
-// var APPIDall="wx859f2afeb6d618c3";//亿邮科技
-// var urlYuming="http://fbinwang.liuhongnan.com"; //线上测试
+urlServer="http://120.27.227.32:8075"; //线上测试
+var APPIDall="wx859f2afeb6d618c3";//亿邮科技
+var urlYuming="http://fbinwang.liuhongnan.com"; //线上测试
 
 collectId=1;
 var productImgUrl=[];
